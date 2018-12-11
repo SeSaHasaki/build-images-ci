e0997209e4bf64dca0d26a0b2bc72b57a5494c09
sangjing <sangjing@leinao.ai>
2018-12-11 13:14:21 +0800

    LNPAI 打印CHANGE_DIR

--- pipeline/ENV.sh
+++ pipeline/ENV.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-export CHANGE_DIR=`git log -1 --name-only -- ../images/ | grep Dockerfile | awk -F "/" '{print$2}'`
+git log -1 --name-only -- ../images/ | grep Dockerfile | awk -F "/" '{print$2}' >> CHANGE_DIR
