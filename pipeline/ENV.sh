82091962eef8782b86bd066fb40047aa20fd2341
sangjing <sangjing@leinao.ai>
2018-12-11 13:32:06 +0800

    LNPAI 测试打包

--- pipeline/ENV.sh
+++ pipeline/ENV.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-echo `git log -1 --name-only -- ../images/ | grep Dockerfile | awk -F "/" '{print$2}'` >> CHANGE_DIR
-echo "已执行sh脚本"
+git log -1 --name-only -- ../images/ | grep Dockerfile | awk -F "/" '{print$2}' >> CHANGE_DIR
