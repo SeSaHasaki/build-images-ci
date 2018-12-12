8eacfeb43b4c4633818bb46250ac2d24425beceb
sangjing <sangjing@leinao.ai>
2018-12-12 18:51:32 +0800

    LNPAI 插入语句

--- pipeline/update_mysql.py
+++ pipeline/update_mysql.py
@@ -14,7 +14,7 @@ imageName = sys.argv[1]
 imageNameSplit = imageName.split('/')
 
 #10.12.4.26:5000/user-images/test_dir:170
-
+a = 1
 # 打开数据库连接
 db = MySQLdb.connect("127.0.0.1", "root", "sangjing", "dataset", charset='utf8' )
 
@@ -26,7 +26,7 @@ cursor = db.cursor()
 try:
    # 执行sql语句
    #cursor.execute(sql)
-   cursor.execute("INSERT INTO images(name,place,description,provider,createtime) VALUES (%s,'10.11.3.8:5000/user-images/sangjing','这是我的镜像描述','sangjing',NOW())",imageNameSplit[2])
+   cursor.execute("INSERT INTO images(name,place,description,provider,createtime) VALUES (%s,'10.11.3.8:5000/user-images/sangjing','这是我的镜像描述','sangjing',NOW())",a)
    # 提交到数据库执行
    db.commit()
 except:
